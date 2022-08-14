b8b720d4ad8f0cef32e7416b2918a9c1e6633d46
twinoxen <1854037+twinoxen@users.noreply.github.com>
2022-08-14 12:45:47 -0400

    attempt to add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa'
+import type { ManifestOptions, VitePWAOptions } from 'vite-plugin-pwa'
+import replace from '@rollup/plugin-replace'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  base: '/',
+  includeAssets: ['favicon.svg'],
+  manifest: {
+    name: 'Food Diary',
+    short_name: 'Food Diary',
+    theme_color: '#e9edde',
+    icons: [
+      {
+        src: 'icon-small.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-big.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'icon-big.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  },
+  devOptions: {
+    enabled: true,
+    navigateFallback: 'index.html',
+  },
+}
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), VitePWA(pwaOptions),],
   server: {
     port: 3000,
   },
